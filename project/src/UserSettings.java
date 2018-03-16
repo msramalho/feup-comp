b6f885c31eac82cab60b81c359389db68811cd40
EdgarACarneiro <up201503784@fe.up.pt>
2018-03-16 00:48:23 +0000

    Ups forgot this files

---
+++ project/src/UserSettings.java
@@ -0,0 +1,22 @@
+public class UserSettings {
+
+    public class Static {
+        public Boolean countFor;
+
+        public Boolean countForeach;
+
+        Static() {}
+    }
+
+    public class Output {
+        public String path;
+
+        Output() {}
+    }
+
+    Static fix;
+
+    Output output;
+
+    UserSettings() {}
+}
