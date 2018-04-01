fa2215442303962476c5af2b50c2aaff2da31c48
msramalho <up201403027@fe.up.pt>
2018-04-01 11:00:54 +0100

    windows bat file run <filename!folder> <[settings_file]>

---
+++ run.bat
@@ -0,0 +1,26 @@
+@echo off
+cls
+prompt --
+mkdir bin
+
+:: parse cmd arguments
+:: get filename or folder from the first argument
+set FILENAME=%1
+
+
+:: get settings file or use the default from the second argument
+set SETTINGS=%2
+IF "%2"=="" (
+    set SETTINGS="project/src/UserSettings.json"
+)
+:: SETTINGS=${SETTINGS:-"../project/src/UserSettings.json"}
+@echo on
+
+
+:: compile source code into bin/
+javac -cp .;gson-2.8.2.jar;./bin/;spoon-core-6.1.0-jar-with-dependencies.jar -d bin project/src/*.java
+echo "Source code compiled"
+
+:: run Main filename settings
+:: usage: <filename|foldername> [<userSettings.json>]
+java -cp .;gson-2.8.2.jar;./bin/;spoon-core-6.1.0-jar-with-dependencies.jar Main "%FILENAME%" "%SETTINGS%"
