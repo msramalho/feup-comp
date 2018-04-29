559feec848fbab48f6f925d5ab29fb192f6d3c44
msramalho <up201403027@fe.up.pt>
2018-04-29 23:26:46 +0100

    extended auto worker with W_testIterator

---
+++ project/src/worker/W_testIterator.java
@@ -0,0 +1,18 @@
+package worker;
+
+import spoon.reflect.declaration.CtElement;
+import spoon.reflect.visitor.Filter;
+import spoon.reflect.visitor.filter.TypeFilter;
+import spoon.support.reflect.code.CtIfImpl;
+
+public class W_testIterator extends AutoWorker {
+
+    public W_testIterator(CtElement element) {
+        super(element);
+    }
+
+    @Override
+    protected Filter setFilter() {
+        return new TypeFilter<>(CtIfImpl.class);
+    }
+}
