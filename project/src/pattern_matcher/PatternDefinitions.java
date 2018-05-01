8c1e23cd3d312f40ab8a0e7a9668e2216143b505
EdgarACarneiro <up201503784@fe.up.pt>
2018-05-01 22:52:38 +0100

    SAving in the map

--- project/src/pattern_matcher/PatternDefinitions.java
+++ project/src/pattern_matcher/PatternDefinitions.java
@@ -3,14 +3,15 @@ package pattern_matcher;
 
 import spoon.Launcher;
 
+import spoon.reflect.code.CtBlock;
 import spoon.reflect.declaration.CtElement;
+import spoon.reflect.declaration.CtMethod;
+import spoon.support.reflect.declaration.CtClassImpl;
 
 import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PatternDefinitions {
     private Map<Class<?>, CtElement> patterns;
@@ -31,10 +32,24 @@ public class PatternDefinitions {
         launcher.getEnvironment().setNoClasspath(true); // Semantic analysis kinda off
         launcher.buildModel();
 
-        List<CtElement> modelElements = launcher.getModel().getElements(null);
-        for (CtElement element : modelElements) {
-            System.out.println(element.getClass().toString() + " --- " + element.toString());
+        //System.out.println(launcher.getModel().);
+        List <CtClassImpl> classElement = launcher.getModel().getElements(ctElement -> (ctElement.getClass() == CtClassImpl.class));
+
+        if (classElement.size() > 1) {
+            System.err.println("There can only be the main class defined in the Patterns file.");
         }
+
+        Set<CtMethod> methods = classElement.get(0).getMethods();
+        // TODO -- Miguel required stuff with methods
+
+        for (CtMethod method: methods) {
+            CtBlock methodBody = method.getBody();
+            System.out.println(methodBody.getStatement(0).getClass() + " ---- " + methodBody);
+            patterns.put(methodBody.getStatement(0).getClass(), methodBody);
+        }
+
+        System.out.println(patterns.values().toString());
+        patterns.clear();
     }
 
     public Map<Class<?>, CtElement> getPatterns() {
