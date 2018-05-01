016b01df2564e0e0ca99e43ba4e5c0d0c35dd6e3
msramalho <up201403027@fe.up.pt>
2018-05-01 10:53:37 +0100

    updated bat file

--- run.bat
+++ run.bat
@@ -13,13 +13,18 @@ set SETTINGS=%2
 IF "%2"=="" (
     set SETTINGS="project/src/UserSettings.json"
 )
+
+set PATTERNS=%3
+IF "%3"=="" (
+    set PATTERNS="project/patterns/Patterns.java"
+)
 @echo on
 
 
 :: compile source code into bin/
-javac -cp .;gson-2.8.2.jar;./bin/;spoon-core-6.1.0-jar-with-dependencies.jar -d bin project/src/main/*.java project/src/worker/*.java project/src/util/*.java
+javac -cp .;gson-2.8.2.jar;./bin/;spoon-core-6.1.0-jar-with-dependencies.jar -d bin project/src/main/*.java project/src/worker/*.java project/src/report/*.java project/src/util/*.java project/src/pattern_matcher/*.java
 echo "Source code compiled"
 
 :: run Main filename settings
 :: usage: <filename|foldername> [<userSettings.json>]
-java -cp .;gson-2.8.2.jar;./bin/;spoon-core-6.1.0-jar-with-dependencies.jar main.Main %FILENAME% "%SETTINGS%"
+java -cp .;gson-2.8.2.jar;./bin/;spoon-core-6.1.0-jar-with-dependencies.jar main.Main %FILENAME% "%SETTINGS%" "%PATTERNS%"
