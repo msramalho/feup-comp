29d6fda862e6c8cade259f5d046a8f3cd698159c
msramalho <up201403027@fe.up.pt>
2018-05-01 09:37:48 +0100

    cleared logger

--- project/src/util/Logger.java
+++ project/src/util/Logger.java
@@ -1,41 +1,20 @@
 package util;
 
 public class Logger {
-    // TODO: maybe allow configuration to specify a log file where we should output the logs, code is commented in Logger
     private Object source; // the object that will call Logger methods
 
 
-    public Logger(Object source) {
-        this.source = source;
-    }
+    public Logger(Object source) { this.source = source; }
 
-    public void print(String message) {
-        System.out.println(format(message));
-    }
+    public void print(String message) { System.out.println(format(message)); }
 
     private String format(String message) {
         // https://stackoverflow.com/a/421338/6196010
         StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+
         // the 3 means this is called from 3 -> 2 (print) -> 1 (format) ->  0 (getStackTrace)
         StackTraceElement relevant = stackTraceElements[3];
         return String.format("[%s->%s:%3d] - %s", source.getClass().getName(), relevant.getMethodName(), relevant.getLineNumber(), message);
     }
 
-    /*
-    public void log(String message){
-        FileOutputStream logStream;
-        try {
-            logStream = new FileOutputStream(logFile, true);
-        } catch (FileNotFoundException e1) {
-            System.err.println("LOGGER: Could not log to file " + logFile.getName());
-            return;
-        }
-
-        String timeStamp = new SimpleDateFormat("yyyy_MM_dd-HH_mm_ss").format(new Date(System.currentTimeMillis()));
-        try {
-            logStream.write((timeStamp + " - " + toLog + "\n").getBytes());
-        } catch (IOException e) {
-            System.err.println("[LOGGER] -  Could not write to log.");
-        }
-    }*/
 }
