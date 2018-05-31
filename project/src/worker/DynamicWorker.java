8077f682458f4d25824b814cf53ed45af01cd9f9
msramalho <up201403027@fe.up.pt>
2018-05-31 17:54:41 +0100

    find variable names automatically and build model with them

--- project/src/worker/DynamicWorker.java
+++ project/src/worker/DynamicWorker.java
@@ -9,11 +9,15 @@ import util.CtIterator;
 
 import spoon.pattern.Pattern;
 
+import java.util.HashSet;
+import java.util.regex.Matcher;
+
 
 public class DynamicWorker extends Worker {
 
 
     private CtElement patternElement;
+    private Pattern pattern;
 
     public DynamicWorker(CtElement rootNode, String patternName, CtElement patternElement) {
         super(rootNode, patternName);
@@ -30,20 +34,38 @@ public class DynamicWorker extends Worker {
     @Override
     public WorkerReport call() throws Exception {
         // logger.print("comparing: " + rootNode.toString() + "\n with pattern: " + patternElement.toString() + " - filter is " + getType().getName());
-
-        Pattern pattern = PatternBuilder.create(patternElement).configureParameters(
-                pb -> {
-                    pb.parameter("_var_x_").byVariable("_var_x_");
-                    pb.parameter("_var_y_").byVariable("_var_y_");
-                    pb.parameter("_var_z_").byVariable("_var_z_");
-                })
-                .build();
+        buildPattern(); // build the pattern from the patternElement
 
         Integer countMatches = pattern.getMatches(rootNode).size();
         if (countMatches >= 1) {
             System.out.println("I got " + countMatches + " match(es) on " + rootNode + "!!");
         }
         return new WorkerReport(countMatches);
+    }
+
+
+    /**
+     * When the Dynamic worker is called, the pattern gets built so that it can be used for matching
+     */
+    private void buildPattern() {
+        pattern = PatternBuilder.create(patternElement).configureParameters(
+                pb -> {
+                    for (String v : getPatternVariables(patternElement.toString()))
+                        pb.parameter(v).byVariable(v);
+                }).build();
+    }
+
+    /**
+     * Find instances of variables in the Patterns file in the format _var_SOMETHING_
+     *
+     * @param code the pattern code as a string
+     * @return a {@link HashSet} of {@link String} with the unique variable names
+     */
+    private HashSet<String> getPatternVariables(String code) {
+        HashSet<String> variables = new HashSet<>();
+        Matcher m = java.util.regex.Pattern.compile("_var_.*?_").matcher(code);
+        while (m.find()) variables.add(m.group(0));
+        return variables;
     }
 
     /*private boolean isAny(CtElement e) {
