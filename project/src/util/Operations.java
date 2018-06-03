9631586edf0286c71a5649fb0d71282a4d608b2a
AndreFCruz <andrecruz97@gmail.com>
2018-06-03 16:43:51 +0100

    median, std, avg: WorkerReport ops

--- project/src/util/Operations.java
+++ project/src/util/Operations.java
@@ -1,9 +1,7 @@
 package util;
 
 import report.WorkerReport;
-
-import java.util.Comparator;
-import java.util.Optional;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Operations {
@@ -13,23 +11,38 @@ public class Operations {
     }
 
     public static Integer max(Stream<WorkerReport> s) {
-        Optional<WorkerReport> max = s.max(WorkerReport::compareTo);
-        return max.isPresent() ? max.get().getValue() : Integer.MIN_VALUE;
+        return s.mapToInt(WorkerReport::getValue).max().orElse(Integer.MIN_VALUE);
     }
 
     public static Integer min(Stream<WorkerReport> s) {
-        Optional<WorkerReport> min = s.min(WorkerReport::compareTo);
-        return min.isPresent() ? min.get().getValue() : Integer.MAX_VALUE;
+        return s.mapToInt(WorkerReport::getValue).min().orElse(Integer.MAX_VALUE);
     }
 
-    // TODO
-    public static Integer median(Stream<WorkerReport> s) {
-        return null;
+    public static Double average(Stream<WorkerReport> s) {
+        return s.mapToInt(WorkerReport::getValue).average().orElse(0);
     }
 
-    // TODO
-    public static Integer standardDeviation(Stream<WorkerReport> s) {
-        return null;
+    public static Double median(Stream<WorkerReport> s) {
+        IntStream sortedValues = s.mapToInt(WorkerReport::getValue).sorted();
+        Long size = s.count();
+        return size % 2 == 0 ?
+                sortedValues.skip(size / 2 - 1).limit(2).average().orElse(0) :
+                sortedValues.skip(size / 2).limit(1).average().orElse(0);
+    }
+
+    /**
+     * Sample Standard Deviation of objects in the provided stream.
+     * @param s the Stream of WorkerReport objects.
+     * @return the sample standard deviation of the given stream of objects.
+     * @see <a href="https://en.wikipedia.org/wiki/Standard_deviation">Standard Deviation</a>
+     */
+    public static Double standardDeviation(Stream<WorkerReport> s) {
+        Double avg = average(s);
+        Double squaredDiff = s.mapToInt(WorkerReport::getValue)
+                .mapToDouble((int i) -> Math.pow(i - avg, 2))
+                .sum();
+
+        return Math.sqrt(squaredDiff / (count(s) - 1));
     }
 
 }
